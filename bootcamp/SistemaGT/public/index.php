479d73146e72fc2bba465897491e67363995a428
Alvaro Saz Márquez <alvaro.anibal.saz@clases.edu.sv>
2025-08-12 09:53:33 -0600

    Add files via upload

---
+++ bootcamp/SistemaGT/public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SistemaGT</title>
+</head>
+<body>
+    <nav>
+        Encabezado del SistemaGT
+
+    </nav>
+    
+    <main>
+        Contenido del Sistema SistemaGT
+    </main>
+ 
+    <footer>
+        Pie de página del SistemaGT
+
+    </footer>
+    
+</body>
+</html>
